61481e2a11dfd85ecc2327d6dd2cb64df49ef802
yanenn <sh5ojkmt@gmail.com>
2023-08-27 15:20:22 +0800

    fix: gh-pages 路徑

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/commonhealth-test/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
